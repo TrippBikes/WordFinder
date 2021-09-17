1989b4384d85625e46b65dd42c5c4a89bee346e4
Tripp <48109302+TrippBikes@users.noreply.github.com>
2021-09-17 11:52:42 -0400

    Faster, Better, Stronger
    Removed the silly nested for loop and imported larger list of English words from nltk.corpus. Code now runs through the list of English words and checks to see if the word matches the criteria instead of brute forcing a bunch of made up words that meet the criteria and comparing them to real words.

--- wordFinder.py
+++ wordFinder.py
@@ -1,74 +1,48 @@
-from english_words import english_words_lower_alpha_set as englwords
+from nltk.corpus import words
+
+# List of English Words
+englWords = words.words()
 
 # Variables
-lowers = "abcdefghijklmnopqrstuvwxyz"
 haveLetters = False
 haveMustUse = False
-foundInvalidLetter = False
 letters = []
 listOWords = []
 
 # Collect letters from user, make sure letters are valid.
 while not haveLetters:
-    askForLetters = input("Available letters: ").lower()
-    letters = list(askForLetters)
-    for letterI in letters:
-        if letterI not in lowers:
-            print(f"Please only use letters, {letterI} is not a letter.")
-            foundInvalidLetter = True
-    if not foundInvalidLetter:
-        haveLetters = True
+    letters = input("Available letters: ").lower()
+    if not letters.isalpha():
+        print(f"Please only use letters, {letters} is not a valid set of letters.")
     else:
-        foundInvalidLetter = False
+        haveLetters = True
 
 # Collect must use letter from user, make sure letter is in provided letters
 while not haveMustUse:
     mustUse = input("Letter that must be used in each word: ").lower()
     if len(mustUse) == 1:
-        if mustUse in askForLetters:
+        if mustUse in letters:
             haveMustUse = True
         else:
-            print(f"Must be one of these letters: {askForLetters}")
+            print(f"Must be one of these letters: {letters}")
     else:
         print("Can only have 1 must use letter.")
 
-
-# Nested for loop tries every possible combination of every letter available against a dictionary and
-# stores it in listOWords if it contains the must use letter. Only collects 4-9 letter words.
-for letterI in letters:
-    for letterII in letters:
-        for letterIII in letters:
-            for letterIV in letters:
-                newWordI = letterI+letterII+letterIII+letterIV
-                if mustUse in newWordI and newWordI in englwords:
-                    listOWords.append(newWordI)
-                for letterV in letters:
-                    newWordII = newWordI+letterV
-                    if mustUse in newWordII and newWordII in englwords:
-                        listOWords.append(newWordII)
-                    for letterVI in letters:
-                        newWordIII = newWordII+letterVI
-                        if mustUse in newWordIII and newWordIII in englwords:
-                            listOWords.append(newWordIII)
-                        for letterVII in letters:
-                            newWordIV = newWordIII+letterVII
-                            if mustUse in newWordIV and newWordIV in englwords:
-                                listOWords.append(newWordIV)
-                            for letterVIII in letters:
-                                newWordV = newWordIV+letterVIII
-                                if mustUse in newWordV and newWordV in englwords:
-                                    listOWords.append(newWordV)
-                                for letterIX in letters:
-                                    newWordVI = newWordV+letterIX
-                                    if mustUse in newWordVI and newWordVI in englwords:
-                                        listOWords.append(newWordVI)
+# Run through each word in englWords, if word is too short or it does not contain must use letter, skip it
+# Check if word only contains the letters available
+for word in englWords:
+    if len(word) < 4 or mustUse not in word:
+        continue
+    else:
+        if set(letters).issuperset(word):
+            listOWords.append(word)
 
 # Alphabetize and check if any words contain all provided letters
 listOWords.sort()
 for word in listOWords:
     containsAllLetters = True
-    for letterI in letters:
-        if letterI in word:
+    for letter in letters:
+        if letter in word:
             continue
         else:
             containsAllLetters = False
@@ -77,7 +51,7 @@ for word in listOWords:
         listOWords.insert(0, f"***{word}***")
 
 # Output words to terminal
-print(f"All Possible 4-9 letter words that include the letter {mustUse}:")
+print(f"All Possible 4-9 letter words that include the letter '{mustUse.upper()}':")
 for word in listOWords:
     print(word)
 
